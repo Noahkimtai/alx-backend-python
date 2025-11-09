60cda21c13cc69bf5e61f695a44390403a3305d6
NoahKimutai <noah.kimutai@ledr.io>
2025-11-09 18:24:27 +0300

    feat: add retry on faile decorator

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,64 @@
+import time
+import sqlite3
+import functools
+
+
+def with_db_connection(func):
+    """decorator to handle opening and closing sqlite3 connection"""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # conn = sqlite3.connect("users.db")
+        # results = func(conn=conn, *args, **kwargs)
+        # conn.close()
+        # return results
+
+        with sqlite3.connect("users.db") as conn:
+            return func(conn=conn, *args, **kwargs)
+
+    return wrapper
+
+
+def retry_on_failure(retries=3, delay=1):
+    """Decorator factory that retries sqlite3 operations"""
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            attempt = 0
+            while attempt < retries:
+                try:
+                    return func(*args, **kwargs)
+                except sqlite3.Error as e:
+                    attempt += 1
+                    print(f"Attempt {attempt} failed: {e}. Retrying in {delay}s...")
+                    time.sleep(delay)
+            # final attempt
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@with_db_connection
+@retry_on_failure(retries=3, delay=1)
+def fetch_users_with_retry(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
+
+@with_db_connection
+@retry_on_failure(retries=3, delay=1)
+def fetch_users_with_retry_fail(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user")
+    return cursor.fetchall()
+
+
+users = fetch_users_with_retry()
+print(users)
+
+users_fail = fetch_users_with_retry_fail()
+print(users)
