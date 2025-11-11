d12c45e2d23d7570ce53669e3e19015821fbde64
NoahKimutai <noah.kimutai@ledr.io>
2025-11-11 12:30:22 +0300

    feat: add fixture, client and README files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
@@ -41,6 +43,17 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+            
+            @TestMemoize
+            def a_property(self):
+                return self.a_method()
 
 
 if __name__ == "__main__":
