aa623ff247aa50962a28bd8d50e25679c76cb1e5
NoahKimutai <noah.kimutai@ledr.io>
2025-11-11 10:53:15 +0300

    test: test access_nested_map function

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+import unittest
+from parameterized import parameterized
+from .utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected):
+        """method to test that the method returns what it is supposed to."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
