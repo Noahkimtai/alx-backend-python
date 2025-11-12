3dbeea10ae8c0c57344098b305a9057cb5a9d8d2
NoahKimutai <noah.kimutai@ledr.io>
2025-11-12 11:53:51 +0300

    feat: add Message, User and Conversation serializer

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+import uuid
+from rest_framework import serializers
+
+from messaging_app.chats.models import Conversation, Message, User
+
+# Create your models here.
+
+
+class UserSerializer(serializers.HyperlinkSerializer):
+    """Represent a user in the messaging app"""
+
+    class meta:
+        model = User
+        fields = [
+            "user_id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "role",
+            "created_at",
+        ]
+        read_only_fields = ["user_id", "created_at"]
+
+
+class MessageSerializer(serializers.HyperlinkSerializer):
+    """Messages between users"""
+
+    sender = UserSerializer(read_only=True)
+
+    class meta:
+        model = Message
+        fields = ["message_id", "sender", "message_body", "sent_at"]
+
+    read_only_fields = ["message_id", "sent_at", "sender"]
+
+
+class ConversationSerializer(serializers.HyperlinkSerializer):
+    """Conversations between users"""
+
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class meta:
+        model = Conversation
+        fields = ["conversation_id", "participants", "messages", "created_at"]
+        read_only_fields = ["conversation_id", "created_at"]
