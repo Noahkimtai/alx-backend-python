f3f94205b573f28bdf5121e001d9a0717c003eca
NoahKimutai <noah.kimutai@ledr.io>
2025-11-13 14:07:19 +0300

    fix: add message viewset

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,6 +3,7 @@ from rest_framework_nested import routers
 
 from .views import ConversationViewSet, MessageViewset, UserViewSet
 
+MessageViewset
 router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewset, basename="message")
