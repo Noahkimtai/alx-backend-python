9016d0edbd96ede2610c9854959e7ffd97713254
NoahKimutai <noah.kimutai@ledr.io>
2025-11-13 10:36:13 +0300

    feat: add api authentication

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -9,4 +9,5 @@ router.register(r"messages", MessageViewset, basename="message")
 router.register(r"users", UserViewSet, basename="user")
 urlpatterns = [
     path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
