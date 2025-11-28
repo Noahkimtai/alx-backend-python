461b41766794b10440c3f775a07182a078551344
NoahKimutai <noah.kimutai@ledr.io>
2025-11-28 20:58:37 +0300

    feat: add pagination page number, next and prev function

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class LargeResultsSetPagination(PageNumberPagination):
@@ -6,14 +7,50 @@ class LargeResultsSetPagination(PageNumberPagination):
     page_size_query_param = "page_size"
     max_page_size = 10000
 
+    def get_paginatied_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "current_page": self.page.number,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
+
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = "page_size"
     max_page_size = 1000
 
+    def get_paginatied_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "current_page": self.page.number,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
+
 
 class SmallResultsSetPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = "page_size"
     pagination_class = 100
+
+    def get_paginatied_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "current_page": self.page.number,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
