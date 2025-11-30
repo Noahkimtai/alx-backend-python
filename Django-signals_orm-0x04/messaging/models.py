280c81a086ed496ea21331b99425a8687d0de1c2
NoahKimutai <noah.kimutai@ledr.io>
2025-11-30 16:31:30 +0300

    feat: add Message and Notification model

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib import User
 
-# Create your models here.
+
+class Message(models.Model):
+
+    sender = models.ForeignKey(
+        User, related_name="sent_message", on_delete=models.CASCADE
+    )
+
+    receiver = models.ForeignKey(
+        User, related_name="received_message", on_delete=models.CASCADE
+    )
+
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender} -> {self.receiver}"
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user}"
